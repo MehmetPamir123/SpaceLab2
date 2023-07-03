f35da8b77f5d602c722f25487c9e4a390a95b43a
Toby Dog <51879119+MehmetPamir123@users.noreply.github.com>
2023-07-03 23:11:05 +0300

    Version0.02
    just adding something..

---
+++ Assets/Joystick.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Joystick : MonoBehaviour,IEndDragHandler,IDragHandler
+{
+    [SerializeField] Canvas canvas;
+    [SerializeField] Transform player;
+    public float maxRadius;
+    RectTransform rectTransform;
+    Vector2 startPos;
+    Vector3 differance = Vector3.zero;
+    float angle;
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startPos = rectTransform.anchoredPosition;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += 2*eventData.delta/canvas.scaleFactor;
+        differance = rectTransform.anchoredPosition - startPos;
+        angle = Mathf.Atan2(differance.y, differance.x);
+        player.transform.localRotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * angle - 90);
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition = startPos;
+        differance = Vector3.zero;
+
+    }
+    private void Update()
+    {
+        
+        
+        if(differance.magnitude <= maxRadius)
+        {
+            MovePlayer(differance,angle);
+        }
+        else
+        {
+
+            Vector2 other = new Vector2(Mathf.Cos(angle) * maxRadius, Mathf.Sin(angle) * maxRadius);
+            MovePlayer(other,angle);
+        }
+    }
+    public void MovePlayer(Vector3 moveVector,float angle)
+    {
+        player.transform.position += moveVector * Time.deltaTime * PlayerStats.Speed * 0.0005f;
+
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, maxRadius);
+    }
+}
---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,14 @@
+public class PlayerStats
+{
+    static public float Speed = 20f;
+    static public float AttackDamage;
+    static public float AbilityDamage;
+    static public float AttackSpeed;
+
+
+    static public float MaxHealth;
+    static public float Fuel;
+    static public float Energy;
+
+
+}
